2e84b0810dc74a9422515438d9d35717b947ecc0
JavedAkhtar1220 <j.akhtar1220@gmail.com>
2022-02-10 22:23:24 +0500

    toggle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
+import imgOnBulb from './images/glow.png';
+import imgOffBulb from './images/off.png';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [bulb, setBulb] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  }
+
+  const switchBulb = () => {
+    setBulb(!bulb);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={darkTheme ? "main-container bg-dark" : "main-container bg-light"}>
+      <div className='toggle-theme shadow'>
+        <button onClick={toggleTheme}>
+          {darkTheme ? <BsFillSunFill /> : <BsFillMoonFill />}
+        </button>
+      </div>
+      <div className='container-bulb'>
+
+        <h2 className={darkTheme ? 'text-white' : 'text-dark'}>Click Bulb to swtich</h2>
+        <div className='mt-3'>
+          {bulb ? <img src={imgOnBulb} onClick={switchBulb} /> : <img src={imgOffBulb} onClick={switchBulb} />}
+
+        </div>
+
+
+      </div>
+
+    </div >
   );
 }
 
